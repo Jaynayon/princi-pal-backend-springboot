994ae795a2964b029cff75220bcd0b59d2ec8f86
Jaynayon <jay.nayonjr@cit.edu>
2024-05-04 05:05:38 +0800

    Remove unused libraries

--- principal/src/main/java/com/it332/principal/Controllers/SchoolController.java
+++ principal/src/main/java/com/it332/principal/Controllers/SchoolController.java
@@ -6,11 +6,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
