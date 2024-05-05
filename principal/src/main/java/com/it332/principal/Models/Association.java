a6a1a0425e86de65e39a0d35d7024ee3c62576f8
kolay404 <angelnicole.b11@gmail.com>
2024-05-05 20:54:32 +0800

    added association

---
+++ principal/src/main/java/com/it332/principal/Models/Association.java
@@ -0,0 +1,48 @@
+package com.it332.principal.Models;
+
+public class Association {
+    private String school;
+    private String user;
+    private boolean approved;
+    private boolean invitation;
+    private boolean admin;
+
+    public Association(String school, String user, boolean approved, boolean invitation, boolean admin) {
+        this.school = school;
+        this.user = user;
+        this.approved = approved;
+        this.invitation = invitation;
+        this.admin = admin;
+    }
+
+    public String getSchool() { 
+        return school;
+    }
+    public void setSchool(String school) {
+        this.school = school;
+    }
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+    public boolean isApproved() {
+        return approved;
+    }
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+    public boolean isInvitation() {
+        return invitation;
+    }
+    public void setInvitation(boolean invitation) {
+        this.invitation = invitation;
+    }
+    public boolean isAdmin() {
+        return admin;
+    }
+    public void setAdmin(boolean admin) [
+        this.admin = admin;
+    ]
+}
