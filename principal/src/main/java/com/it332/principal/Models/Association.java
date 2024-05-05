57eda2f0a06997f5ec320d5a68e2bb9007278f18
Jaynayon <jay.nayonjr@cit.edu>
2024-05-05 21:12:43 +0800

    minor changes

--- principal/src/main/java/com/it332/principal/Models/Association.java
+++ principal/src/main/java/com/it332/principal/Models/Association.java
@@ -3,9 +3,9 @@ package com.it332.principal.Models;
 public class Association {
     private String school;
     private String user;
-    private boolean approved;
-    private boolean invitation;
-    private boolean admin;
+    private boolean approved = false;
+    private boolean invitation = false;
+    private boolean admin = false;
 
     public Association(String school, String user, boolean approved, boolean invitation, boolean admin) {
         this.school = school;
@@ -15,34 +15,43 @@ public class Association {
         this.admin = admin;
     }
 
-    public String getSchool() { 
+    public String getSchool() {
         return school;
     }
+
     public void setSchool(String school) {
         this.school = school;
     }
+
     public String getUser() {
         return user;
     }
+
     public void setUser(String user) {
         this.user = user;
     }
+
     public boolean isApproved() {
         return approved;
     }
+
     public void setApproved(boolean approved) {
         this.approved = approved;
     }
+
     public boolean isInvitation() {
         return invitation;
     }
+
     public void setInvitation(boolean invitation) {
         this.invitation = invitation;
     }
+
     public boolean isAdmin() {
         return admin;
     }
-    public void setAdmin(boolean admin) [
+
+    public void setAdmin(boolean admin) {
         this.admin = admin;
-    ]
+    }
 }
