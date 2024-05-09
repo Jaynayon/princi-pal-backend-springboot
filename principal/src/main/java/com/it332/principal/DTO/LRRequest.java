f4a997ba19f8d38db9b4c425d3ea9c76e0fb59d5
Jaynayon <jay.nayonjr@cit.edu>
2024-05-10 07:46:36 +0800

    Create LRRequest: DTO

---
+++ principal/src/main/java/com/it332/principal/DTO/LRRequest.java
@@ -0,0 +1,102 @@
+package com.it332.principal.DTO;
+
+public class LRRequest {
+    private String id;
+    private String documentsId;
+    private String date;
+    private String orsBursNo;
+    private String particulars;
+    private double amount;
+    private String objectCode;
+    private String payee;
+    private String natureOfPayment;
+
+    // Constructor
+    public LRRequest() {
+    }
+
+    public LRRequest(String date, String orsBursNo, String particulars, double amount, String documentId,
+            String objectCode, String payee, String natureOfPayment) {
+        this.date = date;
+        this.documentsId = documentId;
+        this.orsBursNo = orsBursNo;
+        this.particulars = particulars;
+        this.amount = amount;
+        this.objectCode = objectCode;
+        this.payee = payee;
+        this.natureOfPayment = natureOfPayment;
+    }
+
+    // Getters and setters
+    public String getObjectCode() {
+        return objectCode;
+    }
+
+    public void setObjectCode(String objectCode) {
+        this.objectCode = objectCode;
+    }
+
+    public String getPayee() {
+        return payee;
+    }
+
+    public void setPayee(String payee) {
+        this.payee = payee;
+    }
+
+    public String getNatureOfPayment() {
+        return natureOfPayment;
+    }
+
+    public void setNatureOfPayment(String natureOfPayment) {
+        this.natureOfPayment = natureOfPayment;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getOrsBursNo() {
+        return orsBursNo;
+    }
+
+    public void setOrsBursNo(String orsBursNo) {
+        this.orsBursNo = orsBursNo;
+    }
+
+    public String getParticulars() {
+        return particulars;
+    }
+
+    public void setParticulars(String particulars) {
+        this.particulars = particulars;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDocumentsId() {
+        return documentsId;
+    }
+
+    public void setDocumentsId(String documentsId) {
+        this.documentsId = documentsId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
