bb5f5321015189cdcf7f9ab6ff34ec2c8dbf77d6
Jaynayon <jay.nayonjr@cit.edu>
2024-05-12 07:05:05 +0800

    Add excel DTO

---
+++ principal/src/main/java/com/it332/principal/DTO/ExcelRequest.java
@@ -0,0 +1,13 @@
+package com.it332.principal.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ExcelRequest {
+    private String schoolId;
+    private String documentId;
+    private String month;
+    private String year;
+}
