294f6b0a4d11a25af2e197ffd2ec04a5f243f301
Jaynayon <jay.nayonjr@cit.edu>
2024-05-17 11:52:17 +0800

    Rename AuthController to AuthenticationController

--- principal/src/main/java/com/it332/principal/Controllers/AuthenticationController.java
+++ principal/src/main/java/com/it332/principal/Controllers/AuthenticationController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/authenticate")
-public class AuthController {
+public class AuthenticationController {
 
     @Autowired
     private UserService userService;
