db8b6cab344791e6ca8588cc9ecb2fa603992366
kolay404 <angelnicole.b11@gmail.com>
2024-05-26 19:09:00 +0800

    added cors

--- principal/src/main/java/com/it332/principal/Controllers/AssociationController.java
+++ principal/src/main/java/com/it332/principal/Controllers/AssociationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,6 +20,7 @@ import com.it332.principal.Models.Association;
 import com.it332.principal.Services.AssociationService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/associations")
 public class AssociationController {
 
