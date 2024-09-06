8807c95b0441f0a2447cf043e0110041f4815468
Jaynayon <jay.nayonjr@cit.edu>
2024-09-06 15:25:20 +0800

    Include History ID

--- principal/src/main/java/com/it332/principal/DTO/HistoryResponse.java
+++ principal/src/main/java/com/it332/principal/DTO/HistoryResponse.java
@@ -1,11 +1,5 @@
 package com.it332.principal.DTO;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.it332.principal.Models.History;
 import com.it332.principal.Models.LR;
 
@@ -15,12 +9,13 @@ import lombok.Data;
 @AllArgsConstructor
 @Data
 public class HistoryResponse {
+    private String id;
     private String lrId;
     private UserDetails user;
     private String documentsId;
 
-    @JsonFormat(pattern = "MM/dd/yyyy hh:mm a")
-    private Date updateDate;
+    // @JsonFormat(pattern = "MM/dd/yyyy hh:mm a")
+    private String updateDate;
 
     private String fieldName;
     private String oldValue;
@@ -35,24 +30,15 @@ public class HistoryResponse {
     }
 
     public HistoryResponse(History req) {
+        setId(req.getId());
         setLrId(req.getLrId());
         setDocumentsId(req.getDocumentsId());
+        setUpdateDate(req.getUpdateDate());
         setFieldName(req.getFieldName());
         setOldValue(req.getOldValue());
         setNewValue(req.getNewValue());
         setCreated(req.isCreated());
         setDeleted(req.isDeleted());
         setLrCopy(req.getLrCopy());
-    }
-
-    public String getUpdateDate() {
-        if (updateDate != null) {
-            // Convert UTC Date to PH Time and format it
-            ZonedDateTime utcZonedDateTime = ZonedDateTime.ofInstant(updateDate.toInstant(), ZoneId.of("UTC"));
-            ZonedDateTime phZonedDateTime = utcZonedDateTime.withZoneSameInstant(ZoneId.of("Asia/Manila"));
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm a");
-            return phZonedDateTime.format(formatter); // Format Date object as "MM/dd/yyyy" string
-        }
-        return null; // Return null if date is null (handle this case as needed)
     }
 }
