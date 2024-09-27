67387ff6f68a4d0d4cc7da3eadc80ed460e1b485
Jaynayon <jay.nayonjr@cit.edu>
2024-09-27 11:38:45 +0800

    add WebConfig

---
+++ principal/src/main/java/com/it332/principal/Security/WebConfig.java
@@ -0,0 +1,21 @@
+package com.it332.principal.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://principals.engineer",
+                                "http://localhost:3000") // Back & frontend domains
+                        .allowCredentials(true) // Allow cookies or credentials
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Define allowed HTTP methods
+                        .allowedHeaders("*"); // Allow all headers
+            }
+        };
+    }
+}
