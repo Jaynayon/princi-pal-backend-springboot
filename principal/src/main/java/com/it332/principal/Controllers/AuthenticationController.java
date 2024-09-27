0f60e10f2c48b4f4b917d37a26cef06bfd407667
Jaynayon <jay.nayonjr@cit.edu>
2024-09-27 16:52:29 +0800

    fix cross origin domain

--- principal/src/main/java/com/it332/principal/Controllers/AuthenticationController.java
+++ principal/src/main/java/com/it332/principal/Controllers/AuthenticationController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "https://localhost:3000")
-@CrossOrigin(origins = "https://localhost:3000")
 @RequestMapping("/authenticate")
 public class AuthenticationController {
 
