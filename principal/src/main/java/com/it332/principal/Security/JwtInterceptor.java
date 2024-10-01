3d95dade09cd4f7ac1d99c1e8860a0f83d5aff25
Jaynayon <jay.nayonjr@cit.edu>
2024-10-02 05:14:39 +0800

    add jwt interceptor

--- principal/src/main/java/com/it332/principal/Security/JwtInterceptor.java
+++ principal/src/main/java/com/it332/principal/Security/JwtInterceptor.java
@@ -2,8 +2,6 @@ package com.it332.principal.Security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -32,24 +30,12 @@ public class JwtInterceptor implements HandlerInterceptor {
             // Use JwtTokenService to verify the token
             try {
                 user = jwtTokenService.verifyTokenAndTransform(token);
+                user.getEmail();
             } catch (Exception e) {
                 throw new BadCredentialsException("Invalid JWT Token");
             }
         } else {
             throw new BadCredentialsException("Authorization header is missing or invalid");
-        }
-
-        // Set up the authentication in the SecurityContext if token is valid
-        if (user != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                    user, null, null); // Assuming no roles/authorities for simplicity
-
-            // additional details
-            authToken.setDetails(request.getRemoteAddr());
-
-            SecurityContextHolder.getContext().setAuthentication(authToken);
-        } else {
-            throw new BadCredentialsException("Invalid token or user not found");
         }
 
         return true;
